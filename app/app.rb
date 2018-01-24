e7e0ff0f859b676c365ef8773e14e542a09dd212
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-24 14:09:32 +0000

    Refactor app.rb to reflect the structure

--- app/app.rb
+++ app/app.rb
@@ -1,28 +1,27 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-# We're gonna need our Link model
-require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
   get '/links' do
     # This uses DataMapper's .all method to fetch all
     # data pertaining to this class from the database
     @links = Link.all
-    erb :'links/index'
-  end
-
-  get '/' do
-    'hello world'
-  end
-
-  get '/links/new' do
-    erb :'links/new'
+    erb :'links'
   end
 
   post '/links' do
     Link.create(url: params[:url], title: params[:title])
     redirect '/links'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
   end
 
 end

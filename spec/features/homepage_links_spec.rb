faa973ed01a597193f5c97b00a69732df17d312c
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-24 14:11:39 +0000

    Update test and add test for the homepage

--- spec/features/homepage_links_spec.rb
+++ spec/features/homepage_links_spec.rb
@@ -1,14 +1,14 @@
-feature 'Viewing links' do
+feature 'Opening homepage' do
+  scenario 'homepage displays welcome text' do
+    visit('/')
+    expect(page).to have_content('Welcome to Bookmark Manager!')
+  end
+end
 
+feature 'Viewing links' do
   scenario 'visibility of existing links on the homepage' do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
-    expect(page.status_code).to eq 200
-
-    within 'ul#links' do
-     expect(page).to have_content('Makers Academy')
+    expect(page).to have_content('Makers Academy')
    end
  end
-end
-
-  

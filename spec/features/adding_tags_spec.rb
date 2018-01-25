52448ef7656c97e7b15495f9ceaf3cab65a8ccc6
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-25 16:34:51 +0000

    Add feature test for multiple tags

---
+++ spec/features/adding_tags_spec.rb
@@ -0,0 +1,12 @@
+feature 'Adding tags to the link' do
+  scenario 'I can add multiple tags to the link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    # our tags will be space separated
+    fill_in 'tags',  with: 'education, ruby'
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
+  end
+end

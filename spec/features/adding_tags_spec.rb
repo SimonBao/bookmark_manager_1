ceb13315c7b1173bfbf02714fd44260dfe31dfaf
Simon Bao <Simonbao.sb@gmail.com>
2018-01-26 16:50:46 +0000

    Changed to split tags by comma.

--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -4,9 +4,9 @@ feature 'Adding tags to the link' do
     fill_in 'url',   with: 'http://www.makersacademy.com/'
     fill_in 'title', with: 'Makers Academy'
     # our tags will be space separated
-    fill_in 'tags',  with: 'education ruby'
+    fill_in 'tags',  with: 'search engine, coding, education'
     click_button 'Create link'
     link = Link.first
-    expect(link.tags.map(&:name)).to include('education', 'ruby')
+    expect(link.tags.map(&:name)).to eq(['search engine', 'coding', 'education'])
   end
 end

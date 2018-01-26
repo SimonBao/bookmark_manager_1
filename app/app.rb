7b384d39aa29135f8d6a972e51d7e0a3c2a8c278
Simon Bao <Simonbao.sb@gmail.com>
2018-01-26 16:53:14 +0000

    Sign up page route, helper method creation.

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,9 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
+  enable :sessions
+  set :sessions_secret, 'super secret'
+
   get '/' do
     erb :index
   end
@@ -16,13 +19,25 @@ class BookmarkManager < Sinatra::Base
     erb :'links'
   end
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
   post '/links' do
     link = Link.create(url: params[:url], title: params[:title])
-    params[:tags].split.each do |tag|
+    params[:tags].split(', ').each do |tag|
       link.tags << Tag.first_or_create(name: tag)
     end
     link.save
     redirect to('/links')
+  end
+
+  post '/create_user' do
+    email = params[:email]
+    password = params[:password]
+    user = User.create(email: email, password: password)
+    session[:user_id] = user.id
+    redirect '/links'
   end
 
   get '/links/new' do
@@ -39,5 +54,11 @@ class BookmarkManager < Sinatra::Base
     @links = Link.all
     erb :'/tags'
   end
+
+helper do
+  def current_user
+    @current_user ||= User.get(session[:user_id])
+  end
+end
 
 end
